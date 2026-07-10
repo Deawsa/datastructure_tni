0143829ad028ff8559ccc0b2cbbb2950b9114a60
Phonchanintra Khophol <2513110037@tni.ac.th>
2026-07-10 11:10:01 +0700

    commit

---
+++ LabSheet02/src/Ex06.java
@@ -0,0 +1,25 @@
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		Arraycontroller nums = new Arraycontroller();
+		nums.display("All elements: ");
+		
+		int[] new_nums = new int[nums.getSize()-1];
+		
+		int delete_index = 2;
+		for (int i=0; i<delete_index; i++) {
+			 new_nums[i] = nums.getNums()[i];
+
+		}
+		for (int i=delete_index+1; i<nums.getSize(); i++) {
+			 
+			 new_nums[i - 1] = nums.getNums()[i];
+			}
+		
+		nums.setNums(new_nums);
+		nums.display("\n\nAfter deleted index 2:");
+
+	}
+
+}
